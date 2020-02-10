19cee20b53005e4a4f7667ac89d612bcd3f185c0
Ian Simpson <ian.g.simpson@gmail.com>
2020-02-10 12:33:11 -0800

    add test for auth token parser
    Signed-off-by: Ian Simpson <ian.g.simpson@gmail.com>

--- src/main/java/org/interledger/spsp/server/util/OptionalAuthToken.java
+++ src/main/java/org/interledger/spsp/server/util/OptionalAuthToken.java
@@ -1,10 +1,12 @@
 package org.interledger.spsp.server.util;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public final class OptionalAuthToken {
 
   public static Optional<String> of(Optional<String> authToken) {
+    Objects.requireNonNull(authToken);
     return authToken.map(t -> t.substring(t.indexOf(" ") + 1));
   }
 
