5cc53d20486bba7f0431f29fe660d9cfe8a2e263
nkramer44 <noah.ph.kramer@gmail.com>
2020-03-03 12:35:32 -0500

    cleanup
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>

--- src/main/java/org/interledger/spsp/server/client/AccountSettingsResponse.java
+++ src/main/java/org/interledger/spsp/server/client/AccountSettingsResponse.java
@@ -8,28 +8,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
-/*@JsonSerialize(as = ImmutableAccountSettingsResponse.class)
-@JsonDeserialize(as = ImmutableAccountSettingsResponse.class)
-public interface AccountSettingsResponse extends AccountSettings {
-
-  static ImmutableAccountSettingsResponse.Builder builder() {
-    return ImmutableAccountSettingsResponse.builder();
-  }
-
-  PaymentPointer paymentPointer();
-
-  @Value.Immutable
-  @Value.Modifiable
-  @JsonSerialize(as = ImmutableAccountSettingsResponse.class)
-  @JsonDeserialize(as = ImmutableAccountSettingsResponse.class)
-  abstract class AbstractAccountSettingsResponse implements AccountSettingsResponse {
-
-    @Override
-    public abstract PaymentPointer paymentPointer();
-  }
-}*/
-
-
 @Value.Immutable
 @Value.Modifiable
 @JsonSerialize(as = ImmutableAccountSettingsResponse.class)
